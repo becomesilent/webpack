323d49fdaff7f145e82f4481112830cf04d9e828
zhao <zhao>
2020-05-25 17:25:52 +0800

    webpack

---
+++ src/views/About.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <HelloWorld></HelloWorld>
+  </div>
+</template>
+<script>
+  import HelloWorld from '@/components/HelloWorld.vue'
+  export default {
+    name:'about',
+    data() {
+      return {
+      }
+    },
+    components:{
+      HelloWorld
+    }
+  }
+</script>
+<style></style>
