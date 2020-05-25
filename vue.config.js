318a36e1e0a1c373909254e743f1af90946487eb
zhao <zhao>
2020-05-25 17:58:28 +0800

    +

--- vue.config.js
+++ vue.config.js
@@ -5,7 +5,7 @@ module.exports = {
     pages: {
         index: {
             // page 的入口
-            entry: 'src/index/main.js',
+            entry: 'src/main.js',
             // 模板来源
             template: 'public/index.html',
             // 在 dist/index.html 的输出
@@ -21,7 +21,7 @@ module.exports = {
         // 模板会被推导为 `public/subpage.html`
         // 并且如果找不到的话，就回退到 `public/index.html`。
         // 输出文件名会被推导为 `subpage.html`。
-        subpage: 'src/subpage/main.js'
+        subpage: 'src/main.js'
     }
 }
 // production
